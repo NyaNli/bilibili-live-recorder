62d0aec6e54b2d19dae609637d89a75c3720c6e4
InformationKiller <nyaplus@qq.com>
2019-12-13 20:00:01 +0800

    又更新了……好像增加了更多的画质以及网速匹配

--- BiliLiveRecord.py
+++ BiliLiveRecord.py
@@ -9,8 +9,8 @@ roomno = input('https://live.bilibili.com/') # 也可以直接改成固定直播
 url = 'https://live.bilibili.com/' + roomno
 header = [
     ('User-Agent' , 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36'),
-    ('Accept' , '*/*'),
-    ('Cookie' , "这里填你在live.bilibili.com的Cookie，不要忘记httponly的") # 不填就只有高清可以录
+    ('Accept' , '*/*')
+    # 好像不再需要登录了
 ]
 handler = request.HTTPHandler()
 opener = request.build_opener(handler)
@@ -24,8 +24,22 @@ while True:
         continue
     j = '{' + html.split('window.__NEPTUNE_IS_MY_WAIFU__={')[1].split('</script>')[0]
     room = json.loads(j)
-    if room['roomInitRes']['data']['live_status'] == 1:
-        liveurl = room['roomInitRes']['data']['play_url']['durl'][0]['url'] # 0大概就是主线？
+    if room['roomInitRes']['data']['live_status'] == 1: # 没判断加密码什么的，因为暂时也没遇到
+        roomid = room['roomInitRes']['data']['room_id']
+        qn = 10000 # 不填platform参数原画是4，platform参数填web原画就是10000，以防万一还是先确认一下
+        qualities = room['roomInitRes']['data']['play_url']['quality_description']
+        for q in qualities:
+            if q['desc'] == '原画':
+                qn = q['qn']
+                break
+        try: # 原画画质未必存在html里，要重新获取url了
+            playUrl = json.loads(request.urlopen('https://api.live.bilibili.com/room/v1/Room/playUrl?cid=' + str(roomid) + '&qn=' + str(qn) + '&platform=web').read().decode('utf-8'))
+            if playUrl['code'] != 0:
+                print('Get live stream urls failed: ' + playUrl['message'])
+                continue
+        except HTTPError:
+            continue
+        liveurl = playUrl['data']['durl'][0]['url'] # 0大概就是主线？
         #print(liveurl)
         filename = roomno + '_' + time.strftime("%Y%m%d%H%M%S", time.localtime()) + '.flv'
         print('Recording to file ' + filename)
